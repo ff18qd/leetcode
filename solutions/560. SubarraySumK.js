bbf4fa65eae933019031f4f0e3d9335ea8773dd9
ff18qd <ff18qd@gmail.com>
2018-09-27 21:32:15 -0500

    solve count subarray length is 1

--- solutions/560. SubarraySumK.js
+++ solutions/560. SubarraySumK.js
@@ -10,15 +10,16 @@ var subarraySum = function(nums, k) {
     let result = 0;
     for (let i = 0; i<nums.length; i++) {
         let sum = 0;
-        sum = nums[i];
-        for (let j = i+1; j<nums.length; j++) {
-            sum += nums[j];
+        // sum = nums[i];
+        for (let j = 0; j<nums.length-i; j++) {
+            sum += nums[i+j];
             if (sum === k) {
                 result ++;
                 break;
             }
         }
     }
+  
     return result;
 };
 
